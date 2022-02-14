2cf716a8a4b6db9d182d080bc8f5a9b977fa63fa
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-14 09:33:53 -0600

    Video 10. Event Bubbling

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-var btns = document.querySelectorAll('#book-list .delete')
+const list = document.querySelector('#book-list ul');
 
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const li = e.target.parentElement; //Redirige al elemento padre del boton
-        li.parentNode.removeChild(li); //Elimina al nodo, todo el objeto book
-    });
-});
-
-const link = document.querySelector('#page-banner a');
-link.addEventListener('click', function(e){
-    e.preventDefault(); //Previene la funcion por default
-    console.log("Navigtion to ", e.target.textContent, ' was prevented')
+//delete books
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
 })
