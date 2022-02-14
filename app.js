2ec2b515dec00458cb3f65defc327c0d47a363f1
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-14 00:14:13 -0600

    Video 5. Changing Text & HTML Content

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const wrap = document.querySelector('#book-list li:nth-child(2) .name');
-//console.log(wrap);
-
-var books = document.querySelector('#book-list li .name'); //Todo li con clase name dentro de book-list
-//console.log(books)
-
-books = document.querySelectorAll('#book-list li .name'); //Todo li con clase name dentro de book-list
-//console.log(books)
+let books = document.querySelectorAll('#book-list li .name'); //Todo li con clase name dentro de book-list
 
 Array.from(books).forEach(function(book){
-    console.log(book);
-})
+    book.textContent += " (book title)"; //Reemplazamos el contenido de los li por "test"
+});
+
+const bookList = document.querySelector("#book-list");
+//bookList.innerHTML = '<h2> Books and more books....</h2>';
+bookList.innerHTML += '<p> This is how you add HTML </p>';
