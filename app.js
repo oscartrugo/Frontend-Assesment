cd94f51b75e8f6f70a7db3084e64ee1e6ecd5527
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-14 13:49:18 -0600

    Video 16. Custom Search Filter

--- app.js
+++ app.js
@@ -44,3 +44,18 @@ hideBox.addEventListener('change', function(e){
         list.style.display='initial'; //La vista se restaura
     }
 })
+
+//Filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase(); //Se hace minuscula para que haga match la busqueda
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        if(title.toLocaleLowerCase().indexOf(term)!= -1){
+            book.style.display = 'block';
+        }else{
+            book.style.display = 'none';
+        }
+    })
+});
