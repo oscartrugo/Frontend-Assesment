d4c1947b0ba70143f15571c9a85ca155e9bf1b73
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-14 00:25:03 -0600

    Vdeo 6. Nodes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-let books = document.querySelectorAll('#book-list li .name'); //Todo li con clase name dentro de book-list
+const banner = document.querySelector("#page-banner");
+console.log("#page-banner node type is:", banner.nodeType)
+console.log("#page-banner node name is:", banner.nodeName)
+console.log("#page-banner has child nodes:", banner.hasChildNodes())
 
-Array.from(books).forEach(function(book){
-    book.textContent += " (book title)"; //Reemplazamos el contenido de los li por "test"
-});
-
-const bookList = document.querySelector("#book-list");
-//bookList.innerHTML = '<h2> Books and more books....</h2>';
-bookList.innerHTML += '<p> This is how you add HTML </p>';
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
