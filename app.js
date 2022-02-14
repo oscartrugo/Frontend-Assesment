820e1bfdf9a2c2c77ee0f6ae3e0407bd5dc7a7a3
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-14 10:05:09 -0600

    Video 12. Creating Elements

--- app.js
+++ app.js
@@ -13,5 +13,20 @@ const addForm = document.forms['add-book']
 addForm.addEventListener('submit', function(e){
     e.preventDefault(); //Previene que la pagina haga refresh
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value)
+    
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+
+
+    list.appendChild(li)
 })
