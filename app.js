06a03b85bb1d80c752bf2ca445aed622cb31fb97
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-14 00:37:03 -0600

    Video 7. DOM Traversal (part 1)

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const banner = document.querySelector("#page-banner");
-console.log("#page-banner node type is:", banner.nodeType)
-console.log("#page-banner node name is:", banner.nodeName)
-console.log("#page-banner has child nodes:", banner.hasChildNodes())
-
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+const bookList = document.querySelector('#book-list');
+console.log('the parent node is: ', bookList.parentNode)
+console.log('the parent node is: ', bookList.parentElement.parentElement)
+//Now the children nodes
+console.log(bookList.children);
