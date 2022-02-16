3d73a46380ac01275091fa03c61861ff924a9235
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-16 13:35:47 -0600

    Extending type with interfaces

--- React-TS/typescript-tutorial/src/App.tsx
+++ React-TS/typescript-tutorial/src/App.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number;
+let name: any;
+let age: number | string;
 let isStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
 
-role = [5, 'Oscar']
+let printName: (name:string) => never;
 
+
+// type Person = {
+//   name: string;
+//   age?: number; // ? means its optional, not 100% necesary
+// }
+
+// let person: Person = {
+//   name: 'Oscar',
+//   age: 23
+// }
+
+// let lotsOfPeople: Person[];
+
+//let personName: unknown;
+
+type X={
+  a:string;
+  b:number;
+};
+
+interface Person extends X{
+  name: string;
+  age?: number;
+}
+
+
+
+type Y=X &{
+  c:string;
+  d:number;
+};
 
 function App() {
   return (
