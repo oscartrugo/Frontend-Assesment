cc2280736c01011920879abc2df5cdbd6cdf1da3
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-16 17:58:55 -0600

    Creating Input UI

--- React-TS/typescript-tutorial/src/App.tsx
+++ React-TS/typescript-tutorial/src/App.tsx
@@ -1,49 +1,14 @@
 import React from 'react';
 import './App.css';
-
-let name: any;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-let printName: (name:string) => never;
-
-
-// type Person = {
-//   name: string;
-//   age?: number; // ? means its optional, not 100% necesary
-// }
-
-// let person: Person = {
-//   name: 'Oscar',
-//   age: 23
-// }
-
-// let lotsOfPeople: Person[];
-
-//let personName: unknown;
-
-type X={
-  a:string;
-  b:number;
-};
-
-interface Person extends X{
-  name: string;
-  age?: number;
-}
-
-
-
-type Y=X &{
-  c:string;
-  d:number;
-};
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
   return (
-    <span className="heading">Taskify</span>
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField />
+    </div>
+    
   );
 }
 
