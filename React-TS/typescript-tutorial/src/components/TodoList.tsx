0febbb89f354104146da5e079f2eca770a3f33f6
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-16 18:46:54 -0600

    todo list component

--- React-TS/typescript-tutorial/src/components/TodoList.tsx
+++ React-TS/typescript-tutorial/src/components/TodoList.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import './styles.css'
+import { Todo } from "../model";
+import SingleTodo from './SingleTodo';
 
-const TodoList = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+interface props {
+    todos: Array<Todo>;
+    setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+    CompletedTodos: Array<Todo>;
+  }
+  
+
+const TodoList:React.FC = (todos, set) => {
+  return ( 
+  <div className='todos'>{
+      todos.map((todo) => (
+        <SingleTodo
+        index={index}
+        todos={todos}
+        todo={todo}
+        key={todo.id}
+        setTodos={setTodos}
+      />
+      ))}
+  </div>
+  );
 }
 
 export default TodoList
