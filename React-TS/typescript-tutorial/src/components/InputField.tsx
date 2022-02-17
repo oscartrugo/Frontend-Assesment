51095290d20e182992396e389c659165b58b6fe1
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-16 18:28:59 -0600

    useRef

--- React-TS/typescript-tutorial/src/components/InputField.tsx
+++ React-TS/typescript-tutorial/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css';
 
 interface props {
@@ -6,21 +6,32 @@ interface props {
     setTodo: React.Dispatch<React.SetStateAction<string>>;
     handleAdd: (e: React.FormEvent) => void;
   }
-
-const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  
+  const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+  
     return (
-        <form action="" className="input" onSubmit={handleAdd}>
-            <input
-                type="text"
-                placeholder="Enter a Task"
-                value={todo}
-                ref={inputRef}
-                onChange={(e) => setTodo(e.target.value)}
-                className="input__box"
-            />
-            <button className="input__submit" type='submit'>Go</button>
-        </form>
-    )
-}
+      <form
+        className="input"
+        onSubmit={(e) => {
+          handleAdd(e);
+          inputRef.current?.blur();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter a Task"
+          value={todo}
+          ref={inputRef}
+          onChange={(e) => setTodo(e.target.value)}
+          className="input__box"
+        />
+        <button type="submit" className="input_submit">
+          GO
+        </button>
+      </form>
+    );
+  };
+  
 
 export default InputField
