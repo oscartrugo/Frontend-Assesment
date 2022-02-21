a83026a12b20f04b69552ce28f99bca9a6f1d989
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-20 19:56:02 -0600

    Create Backend API

--- Pa11/index.js
+++ Pa11/index.js
@@ -1,8 +1,18 @@
+const express = require('express');
 const pa11y = require('pa11y');
+const PORT = process.env.PORT || 5000;
 
-async function run() {
-    const response = await pa11y('https://traversy.dev');
-    console.log(response);
-}
+const app = express();
 
-run()
+app.use(express.static('public'));
+
+app.get('/api/test', async (req, res) => {
+    if(!req.query.url){
+        res.status(400).json({ error: 'url is required' });
+    }else{
+        const results = await pa11y(req.query.url);
+        res.status(200).json(results);
+    }
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
